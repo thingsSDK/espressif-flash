5b5e27ce50971122abab4c60376c8565212d26ea
Andrew Chalkley <andrew@chalkley.org>
2018-04-28 15:41:36 -0700

    Got the sync command working

--- main.js
+++ main.js
@@ -1,22 +1,79 @@
-import delay from './delay'
-import SerialPort from 'serialport'
+import delay from "./delay";
+import SerialPort from "serialport";
+const syncFrame = [
+  0xc0,
+  0x00,
+  0x08,
+  0x24,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x07,
+  0x07,
+  0x12,
+  0x20,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0x55,
+  0xc0
+];
 
 const bootloader = async port => {
+  await delay(0);
+  port.set({ rts: true, dtr: false });
+  await delay(5);
+  port.set({ rts: false, dtr: true });
+  await delay(50);
+  port.set({ rts: false, dtr: false });
+  sync(port);
+};
 
-  await delay(0)
-  port.set({rts: true, dtr: false})
-  await delay(5)
-  port.set({rts: false, dtr: true})
-  await delay(50)
-  port.set({rts: true, dtr: false})
+const sync = port => {
+  port.flush(() => {
+    port.write([...syncFrame], "chunk", async () => {
+      port.write([...syncFrame], "chunk", async () => {
+        const data = port.read(12*128);
+        console.dir(data);
+      });
+    });
+  });
+};
 
-}
+const port = new SerialPort("COM12", {
+  baudRate: 115200
+});
 
-const port = new SerialPort('COM12', {
-  baudRate: 115200,
-})
-
-
-port.on('open', () => {
-  bootloader(port)
-})
+port.on("open", () => {
+  bootloader(port);
+});
